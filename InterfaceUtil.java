8f969a5693bcca8135821e5d6f20c59268ff32ce
Jeff Pace <jeugenepace@gmail.com>
2011-09-03 11:30:25 -0400

    First commit.

---
+++ InterfaceUtil.java
@@ -0,0 +1,13 @@
+package org.incava.pmdx;
+
+import java.util.*;
+import net.sourceforge.pmd.ast.*;
+
+
+/**
+ * Miscellaneous routines for interfaces.
+ */
+public class InterfaceUtil extends ClassUtil {
+
+}
+
