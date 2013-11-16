643459d2b41350ac496ab138ea6747aa99c84f75
Jeff Pace <jeugenepace@gmail.com>
2013-11-16 17:55:50 -0500

    Added abstractions from PMD.
    This change added TypeDeclarationList, wrapping the List<Class/InterfaceDecl>
    code.

--- TypeDeclarationUtil.java
+++ TypeDeclarationUtil.java
@@ -1,9 +1,6 @@
 package org.incava.pmdx;
 
 import java.util.List;
-import java.util.Map;
-import net.sourceforge.pmd.ast.ASTClassOrInterfaceBody;
-import net.sourceforge.pmd.ast.ASTClassOrInterfaceBodyDeclaration;
 import net.sourceforge.pmd.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.ast.ASTTypeDeclaration;
 import net.sourceforge.pmd.ast.SimpleNode;
@@ -33,39 +30,5 @@ public class TypeDeclarationUtil extends SimpleNodeUtil {
         }
 
         return null;
-    }
-
-    /**
-     * Returns a list of all methods, fields, constructors, and inner classes
-     * and interfaces.
-     */
-    public static List<ASTClassOrInterfaceBodyDeclaration> getDeclarations(ASTTypeDeclaration tdecl) {
-        ASTClassOrInterfaceDeclaration cidecl = findChild(tdecl, ASTClassOrInterfaceDeclaration.class);
-        return getDeclarations(cidecl);
-    }
-
-    /**
-     * Returns a list of all methods, fields, constructors, and inner classes
-     * and interfaces.
-     */
-    public static List<ASTClassOrInterfaceBodyDeclaration> getDeclarations(ASTClassOrInterfaceDeclaration coid) {
-        ASTClassOrInterfaceBody body = findChild(coid, ASTClassOrInterfaceBody.class);
-        return findChildren(body, ASTClassOrInterfaceBodyDeclaration.class);
-    }
-
-    /**
-     * Returns the real declaration, which is a method, field, constructor, or
-     * inner class or interface.
-     */
-    public static SimpleNode getDeclaration(ASTClassOrInterfaceBodyDeclaration bdecl) {
-        return hasChildren(bdecl) ? findChild(bdecl) : null;
-    }
-
-    /**
-     * Returns the real declaration, which is a method, field, constructor, or
-     * inner class or interface.
-     */
-    public static <NodeType extends SimpleNode> NodeType getDeclaration(ASTClassOrInterfaceBodyDeclaration bdecl, Class<NodeType> cls) {
-        return hasChildren(bdecl) ? findChild(bdecl, cls) : null;
     }
 }
