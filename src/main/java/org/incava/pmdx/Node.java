9c863ba8b953fdbd52de4aadcb1435cfbafb371a
Jeff Pace <jeugenepace@gmail.com>
2017-07-05 19:28:14 -0400

    Add Node class, instance version of SimpleNodeUtil.

---
+++ src/main/java/org/incava/pmdx/Node.java
@@ -0,0 +1,351 @@
+package org.incava.pmdx;
+
+import java.util.ArrayList;
+import java.util.List;
+import net.sourceforge.pmd.lang.ast.GenericToken;
+import net.sourceforge.pmd.lang.java.ast.AbstractJavaNode;
+import net.sourceforge.pmd.lang.java.ast.JavaParserConstants;
+import net.sourceforge.pmd.lang.java.ast.Token;
+
+import static org.incava.ijdk.util.IUtil.*;
+
+/**
+ * Miscellaneous functions for AbstractJavaNode.
+ */
+public class Node {
+    private final AbstractJavaNode node;
+
+    public Node(AbstractJavaNode node) {
+        this.node = node;
+    }
+    
+    /**
+     * Returns the token images for the node.
+     */
+    public String toString() {
+        Token tk = getFirstToken();
+        Token last = getLastToken();
+        StringBuilder sb = new StringBuilder(tk.image);
+        while (tk != last) {
+            tk = tk.next;
+            sb.append(tk.image);
+        }
+        return sb.toString();
+    }
+
+    public Token getFirstToken() {
+        return (Token)node.jjtGetFirstToken();
+    }
+
+    public Token getLastToken() {
+        return (Token)node.jjtGetLastToken();
+    }
+
+    /**
+     * Returns whether the node has any children.
+     */
+    public boolean hasChildren() {
+        return node.jjtGetNumChildren() > 0;
+    }
+
+    /**
+     * Returns the parent node.
+     */
+    public AbstractJavaNode getParent() {
+        return (AbstractJavaNode)node.jjtGetParent();
+    }
+
+    /**
+     * Returns a list of children, both nodes and tokens.
+     */
+    public List<Object> getChildren() {
+        return getChildren(true, true);
+    }
+
+    /**
+     * Returns a list of children, optionally nodes and tokens.
+     */
+    public List<Object> getChildren(boolean getNodes, boolean getTokens) {
+        List<Object> list = new ArrayList<Object>();
+        
+        Token t = new Token();
+        t.next = getFirstToken();
+        
+        int nChildren = node.jjtGetNumChildren();
+        for (int ord = 0; ord < nChildren; ++ord) {
+            AbstractJavaNode n = (AbstractJavaNode)node.jjtGetChild(ord);
+            Node nn = new Node(n);
+            while (true) {
+                t = t.next;
+                if (t == nn.getFirstToken()) {
+                    break;
+                }
+                if (getTokens) {
+                    list.add(t);
+                }
+            }
+            if (getNodes) {
+                list.add(n);
+            }
+            t = nn.getLastToken();
+        }
+
+        while (t != getLastToken()) {
+            t = t.next;
+            if (getTokens) {
+                list.add(t);
+            }
+        }
+
+        return list;
+    }
+
+    /**
+     * Returns the first child node of the parent.
+     */
+    public AbstractJavaNode findChild() {
+        return findChild(null);
+    }
+
+    /**
+     * Returns the first child node of the parent, with the given type.
+     */
+    public <NodeType extends AbstractJavaNode> NodeType findChild(Class<NodeType> childType) {
+        return findChild(childType, 0);
+    }
+
+    /**
+     * Returns the nth child node of the parent, with the given type.
+     */
+    @SuppressWarnings("unchecked")
+    public <NodeType extends AbstractJavaNode> NodeType findChild(Class<NodeType> childType, int nth) {
+        if (nth < 0 || isNull(node)) {
+            return null;
+        }
+
+        int nChildren = node.jjtGetNumChildren();
+        if (nth >= nChildren) {
+            return null;
+        }
+
+        int nFound = -1;
+        for (int idx = 0; idx < nChildren; ++idx) {
+            AbstractJavaNode child = getChildOfType(childType, idx);
+            if (isNotNull(child) && ++nFound == nth) {
+                return (NodeType)child;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns a list of child tokens, non-hierarchically.
+     */
+    public List<Token> getChildTokens() {
+        List<Token> children = new ArrayList<Token>();
+        
+        Token tk = getFirstToken();
+        Token lastTk = getLastToken();
+        
+        while (tk != null) {
+            children.add(tk);
+            if (tk == lastTk) { // yes, ==, not equals
+                break;
+            }
+            tk = tk.next;
+        }
+
+        return children;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <NodeType extends AbstractJavaNode> List<NodeType> findChildren(Class<NodeType> childType) {
+        List<NodeType> list = new ArrayList<NodeType>();
+        int nChildren = node == null ? 0 : node.jjtGetNumChildren();
+        for (int i = 0; i < nChildren; ++i) {
+            AbstractJavaNode child = (AbstractJavaNode)node.jjtGetChild(i);
+            if (childType == null || child.getClass().equals(childType)) {
+                list.add((NodeType)child);
+            }
+        }
+        return list;
+    }
+
+    // apparently unused (by DiffJ)
+    // @SuppressWarnings("unchecked")
+    // public static <NodeType extends AbstractJavaNode> List<NodeType> findChildren(AbstractJavaNode parent) {
+    //     return findChildren(parent, null);
+    // }
+
+    // /**
+    //  * Returns the tokens for a node.
+    //  */
+    // public static List<Token> getTokens(AbstractJavaNode node) {
+    //     List<Token> tokens = new ArrayList<Token>();
+    //     Token tk = new Token();
+    //     tk.next = getFirstToken(node);
+
+    //     if (tk != null) {
+    //         tokens.add(tk);
+    //         do {
+    //             tk = tk.next;
+    //             tokens.add(tk);
+    //         } while (tk != getLastToken(node));
+    //     }
+    //     return tokens;
+    // }
+
+    public Token findToken(int tokenType) {
+        List<Object> childTokens = getChildren(false, true);
+        for (Object obj : childTokens) {
+            Token tk = (Token)obj;
+            if (tk.kind == tokenType) {
+                return tk;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns whether the node has a matching token, occurring prior to any non-tokens (i.e.,
+     * before any child nodes).
+     */
+    public boolean hasLeadingToken(int tokenType) {
+        return getLeadingToken(tokenType) != null;
+    }
+
+    /**
+     * Returns the matching token, occurring prior to any non-tokens (i.e., before any child nodes).
+     */
+    public Token getLeadingToken(int tokenType) {
+        if (node.jjtGetNumChildren() == 0) {
+            return null;
+        }
+
+        AbstractJavaNode n = (AbstractJavaNode)node.jjtGetChild(0);
+
+        Token t = new Token();
+        t.next = getFirstToken();
+            
+        while (true) {
+            t = t.next;
+            Node nn = new Node(n);
+            if (t == nn.getFirstToken()) {
+                break;
+            }
+            else if (t.kind == tokenType) {
+                return t;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns the tokens preceding the first child of the node.
+     */
+    public List<Token> getLeadingTokens() {
+        List<Token> list = new ArrayList<Token>();
+        
+        if (node.jjtGetNumChildren() == 0) {
+            return list;
+        }
+
+        AbstractJavaNode n = (AbstractJavaNode)node.jjtGetChild(0);
+
+        Token t = new Token();
+        t.next = getFirstToken();
+            
+        while (true) {
+            t = t.next;
+            Node nn = new Node(n);
+            if (t == nn.getFirstToken()) {
+                break;
+            }
+            else {
+                list.add(t);
+            }
+        }
+
+        return list;
+    }
+
+    public void print() {
+        print("");
+    }
+
+    public void print(String prefix) {
+        Token first = getFirstToken();
+        Token last  = getLastToken();
+        tr.Ace.log(prefix + node.toString() + ":" + TokenUtil.getLocation(first, last));
+    }
+
+    public void dump() {
+        dump("", false);
+    }
+
+    public void dump(String prefix) {
+        dump(prefix, false);
+    }
+
+    public void dump(String prefix, boolean showWhitespace) {
+        print(prefix);
+
+        List<Object> children = getChildren();
+        for (Object obj : children) {
+            dumpObject(obj, prefix, showWhitespace);
+        }
+    }  
+
+    public void dumpObject(Object obj, String prefix, boolean showWhitespace) {
+        if (obj instanceof Token) {
+            Token tk = (Token)obj;                
+            if (showWhitespace && tk.specialToken != null) {
+                TokenUtil.dumpToken(tk.specialToken, prefix);
+            }                
+            tr.Ace.log(prefix + "    \"" + tk + "\" " + TokenUtil.getLocation(tk, tk) + " (" + tk.kind + ")");
+        }
+        else {
+            AbstractJavaNode sn = (AbstractJavaNode)obj;
+            Node nn = new Node(sn);
+            nn.dump(prefix + "    ", showWhitespace);
+        }
+    }
+
+    /**
+     * Returns a numeric "level" for the node. Zero is public or abstract, one is protected, two is
+     * package, and three is private.
+     */
+    public int getLevel() {
+        List<Token> tokens = getLeadingTokens();
+        for (Token t : tokens) {
+            switch (t.kind) {
+                case JavaParserConstants.PUBLIC:
+                    // fallthrough
+                case JavaParserConstants.ABSTRACT:
+                    return 0;
+                case JavaParserConstants.PROTECTED:
+                    return 1;
+                case JavaParserConstants.PRIVATE:
+                    return 3;
+            }
+        }
+
+        // AKA "package"
+        return 2;
+    }
+
+    private String getClassName(Class<?> cls) {
+        return cls == null ? null : cls.getName();
+    }
+
+    /**
+     * Returns the node if the class of the child at the given index matches the given class type.
+     * If the given one is null, the child will match.
+     */
+    @SuppressWarnings("unchecked")
+    private <NodeType extends AbstractJavaNode> NodeType getChildOfType(Class<NodeType> childType, int index) {
+        AbstractJavaNode child = (AbstractJavaNode)node.jjtGetChild(index);
+        return isNull(childType) || child.getClass().equals(childType) ? (NodeType)child : null;
+    }    
+}
---
+++ src/test/java/org/incava/pmdx/NodeTest.java
@@ -0,0 +1,205 @@
+package org.incava.pmdx;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.List;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
+import net.sourceforge.pmd.lang.ast.JavaCharStream;
+import net.sourceforge.pmd.lang.ast.TokenMgrError;
+import net.sourceforge.pmd.lang.java.ast.ASTCompilationUnit;
+import net.sourceforge.pmd.lang.java.ast.ASTImportDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTPackageDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTTypeDeclaration;
+import net.sourceforge.pmd.lang.java.ast.AbstractJavaNode;
+import net.sourceforge.pmd.lang.java.ast.JavaParser;
+import net.sourceforge.pmd.lang.java.ast.JavaParserConstants;
+import net.sourceforge.pmd.lang.java.ast.ParseException;
+import net.sourceforge.pmd.lang.java.ast.Token;
+import org.incava.attest.Parameterized;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.incava.attest.Assertions.assertEqual;
+import static org.incava.attest.Assertions.message;
+import static org.incava.attest.ContextMatcher.withContext;
+
+public class NodeTest extends Parameterized {
+    private static ASTCompilationUnit compUnit;
+    private static Node node;
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        compUnit = compile("package abc;\nclass C1 {}");
+        node = new Node(compUnit);
+    }
+
+    private static ASTCompilationUnit compile(String str) throws Exception {
+        Reader reader = new StringReader(str);
+        JavaCharStream jcs = new JavaCharStream(reader);
+        JavaParser parser = new JavaParser(jcs);
+
+        // 1.6:
+        parser.setJdkVersion(6);
+
+        return parser.CompilationUnit();
+    }
+    
+    @Test
+    public void getFirstToken() {
+        Token tk = node.getFirstToken();
+
+        assertThat(tk.toString(), equalTo("package"));
+        assertThat(tk.kind, equalTo(43));
+    }
+    
+    @Test
+    public void getLastToken() {
+        Token tk = node.getLastToken();
+
+        // a compilation unit has two EOFs?
+
+        assertThat(tk.toString(), equalTo(""));
+        assertThat(tk.kind, equalTo(0));
+    }
+    
+    @Test
+    public void hasChildren() {
+        boolean result = node.hasChildren();
+        assertThat(result, equalTo(true));
+    }
+    
+    @Test
+    public void getParent() {
+        AbstractJavaNode parent = node.getParent();
+        assertThat(parent, nullValue());
+    }
+    
+    @Test
+    public void getChildren() {
+        List<Object> children = node.getChildren();
+
+        assertThat(children, hasSize(3));
+        assertThat(children.get(0), instanceOf(ASTPackageDeclaration.class));
+        assertThat(children.get(1), instanceOf(ASTTypeDeclaration.class));
+        assertThat(children.get(2), instanceOf(Token.class));        
+    }
+    
+    @Test
+    public void getChildrenNodes() {
+        List<Object> children = node.getChildren(true, false);
+
+        assertThat(children, hasSize(2));
+        assertThat(children.get(0), instanceOf(ASTPackageDeclaration.class));
+        assertThat(children.get(1), instanceOf(ASTTypeDeclaration.class));
+    }
+    
+    @Test
+    public void getChildrenTokens() {
+        List<Object> children = node.getChildren(false, true);
+
+        assertThat(children, hasSize(1));
+        assertThat(children.get(0), instanceOf(Token.class));        
+    }
+    
+    @Test
+    public void getChildrenNodesTokens() {
+        List<Object> children = node.getChildren(true, true);
+
+        assertThat(children, hasSize(3));
+        assertThat(children.get(0), instanceOf(ASTPackageDeclaration.class));
+        assertThat(children.get(1), instanceOf(ASTTypeDeclaration.class));
+        assertThat(children.get(2), instanceOf(Token.class));        
+    }
+    
+    @Test
+    public void getChildrenNeither() {
+        List<Object> children = node.getChildren(false, false);
+        assertThat(children, empty());
+    }    
+    
+    @Test
+    public void findChildNoArgs() {
+        AbstractJavaNode child = node.findChild();
+        assertThat(child, instanceOf(ASTPackageDeclaration.class));
+    }    
+    
+    @Test
+    public void findChildOfClass() {
+        AbstractJavaNode pkg = node.findChild(ASTPackageDeclaration.class);
+        assertThat(pkg, instanceOf(ASTPackageDeclaration.class));
+
+        AbstractJavaNode type = node.findChild(ASTTypeDeclaration.class);
+        assertThat(type, instanceOf(ASTTypeDeclaration.class));        
+    }
+    
+    @Test
+    public void findChildOfClassOfCount() {
+        AbstractJavaNode pkg0 = node.findChild(ASTPackageDeclaration.class, 0);
+        assertThat(pkg0, instanceOf(ASTPackageDeclaration.class));
+
+        AbstractJavaNode pkg1 = node.findChild(ASTPackageDeclaration.class, 1);
+        assertThat(pkg1, nullValue());
+    }
+    
+    @Test
+    public void getChildTokens() {
+        List<Token> tokens = node.getChildTokens();
+
+        assertThat(tokens, hasSize(8));
+        assertThat(tokens.get(0), hasToString("package"));
+        assertThat(tokens.get(1), hasToString("abc"));
+        assertThat(tokens.get(2), hasToString(";"));
+        assertThat(tokens.get(3), hasToString("class"));
+        assertThat(tokens.get(4), hasToString("C1"));
+        assertThat(tokens.get(5), hasToString("{"));
+        assertThat(tokens.get(6), hasToString("}"));
+        assertThat(tokens.get(7), hasToString(""));
+
+        // the second EOF is not included
+    }
+
+    private <NodeType extends AbstractJavaNode> void assertFindChildren(int numExpected, ASTCompilationUnit cu, Class<NodeType> childType) {
+        Node n = new Node(cu);
+        List<NodeType> children = n.findChildren(childType);
+        assertThat(children, hasSize(numExpected));
+    }
+
+    @Test
+    public void findChildren() throws Exception {
+        ASTCompilationUnit cu = compile("package abc;\nclass C {}\nclass D {}");
+        assertFindChildren(1, cu, ASTPackageDeclaration.class);
+        assertFindChildren(2, cu, ASTTypeDeclaration.class);
+        assertFindChildren(0, cu, ASTImportDeclaration.class);
+    }
+
+    @Test
+    public void findToken() throws Exception {
+        ASTCompilationUnit cu = compile("package abc;\npublic class C {}");
+        AbstractJavaNode type = new Node(cu).findChild(ASTTypeDeclaration.class);
+
+        Token pb = SimpleNodeUtil.findToken(type, JavaParserConstants.PUBLIC);
+        assertThat(pb, notNullValue());
+
+        Token abs = SimpleNodeUtil.findToken(type, JavaParserConstants.ABSTRACT);
+        assertThat(abs, nullValue());
+    }
+
+    @Test
+    public void getLeadingToken() throws Exception {
+        ASTCompilationUnit cu = compile("package abc;\npublic abstract class C {}");
+        AbstractJavaNode type = new Node(cu).findChild(ASTTypeDeclaration.class);
+        Node n = new Node(type);
+
+        Token pb = n.getLeadingToken(JavaParserConstants.PUBLIC);
+        assertThat(pb, notNullValue());
+
+        Token abs = n.getLeadingToken(JavaParserConstants.ABSTRACT);
+        assertThat(abs, notNullValue());
+
+        Token st = n.findToken(JavaParserConstants.STATIC);
+        assertThat(st, nullValue());
+    }    
+}
